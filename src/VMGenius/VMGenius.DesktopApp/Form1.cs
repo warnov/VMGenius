26cc8e1d1aef9388d33f9bed5665de3a0269f15c
warnov <warnov@gmail.com>
2022-09-10 17:07:46 -0500

    Main Project Structure Created

---
+++ src/VMGenius/VMGenius.DesktopApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace VMGenius.DesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/VMGenius/VMGenius.Types/VMType.cs
@@ -0,0 +1,9 @@
+﻿namespace VMGenius.Types;
+
+public class VMType
+{
+    public string? Type { get; set; }
+    public string? Link { get; set; }
+    public string? Sizes { get; set; }
+    public string? Description { get; set; }
+}
