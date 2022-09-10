3070dbc4057ce48075fd320649a64b44899f4860
warnov <warnov@gmail.com>
2022-09-10 18:11:39 -0500

    FX Project Added

---
+++ src/VMGenius/VMGenius.FX/VMTypes.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+
+namespace VMGenius.FX
+{
+    public static class VMTypes
+    {
+        [FunctionName("GetVMTypes")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            var vmTypesURL = Environment.GetEnvironmentVariable("VMTypesURL");
+            HttpClient wc = new();
+            var vmTypesJson = wc.GetStringAsync(vmTypesURL).Result;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
