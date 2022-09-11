6f151df3de99d246d57b584e73429cc295fc9f1b
warnov <warnov@hotmail.com>
2022-09-11 00:00:10 -0500

    GetVMTypesFX Finished OK Reading from public Blob

--- src/VMGenius/VMGenius.FX/VMTypes.cs
+++ src/VMGenius/VMGenius.FX/VMTypes.cs
@@ -1,32 +1,85 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using VMGenius.Types;
 
-namespace VMGenius.FX
+
+
+
+namespace VMGenius.FX;
+public static class VMTypes
 {
-    public static class VMTypes
+
+
+
+
+    [FunctionName("GetVMTypes")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "req even when not used is required for the FX runtime to work")]
+    public static IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
     {
-        [FunctionName("GetVMTypes")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-            ILogger log)
+
+
+
+
+        //Downloading VM Types JSON
+        //===========================
+
+        var vmTypesURL = Environment.GetEnvironmentVariable("VMTypesURL");
+        
+        //>>  Parameter not found
+        if (vmTypesURL == null)
+            return new ConflictObjectResult(E.CONFIGURATION_PARAMETER_NOT_FOUND);
+
+
+        HttpClient wc = new();
+        string vmTypesJson;
+        try
         {
-
-            var vmTypesURL = Environment.GetEnvironmentVariable("VMTypesURL");
-            HttpClient wc = new();
-            var vmTypesJson = wc.GetStringAsync(vmTypesURL).Result;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            vmTypesJson = wc.GetStringAsync(vmTypesURL).Result;
         }
+        catch
+        {
+            //>> Origin of data not found
+            return new NotFoundObjectResult(E.INVALID_VMTYPES_SOURCE);
+        }
+        
+       
+
+
+
+        //Deserializing VM Types JSON
+        //===========================
+
+        List<VMType> vmTypes;
+        try
+        {
+            vmTypes = JsonConvert.DeserializeObject<List<VMType>>(vmTypesJson);
+        }
+        catch
+        {
+            //>> Invalid JSON Response
+            return new ConflictObjectResult(E.INVALID_VMTYPES_RESPONSE);
+        }
+
+
+
+
+
+        //>>  Deserialization yielded null
+        if (vmTypes == null) return new NotFoundObjectResult(E.EMPTY_VMTYPES_RESPONSE);
+
+
+
+
+
+        //>>  Returning the VM Types (FX auto serialize it again to JSON)
+        return new OkObjectResult(vmTypes);
+
     }
 }
